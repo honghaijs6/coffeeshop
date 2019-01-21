f1db1609fca3af66448d797e8206488ba3b522cc
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2019-01-22 01:30:11 +0700

    Create BenTabs.js

---
+++ src/components/BenTabs.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class BenTabs extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            onTab:'',
+        }
+    }
+    render() {
+        return (
+            <View>
+                <Text>asdasd</Text>
+            </View>
+        );
+    }
+}
+
+export default BenTabs;
