9cb51ffa9a51571cc98cdfec1afc04a0d1fd6217
Hồng Hải <honghai.js6@gmail.com>
2019-01-24 18:22:55 +0700

    Update const.js

--- src/config/const.js
+++ src/config/const.js
@@ -1,4 +1,4 @@
-export const COFFEE_COLOR = '#904E1A';
+export const COFFEE_COLOR = '#18A689';
 export const GREY_COLOR = '#F0F0F0';
-export const BLACK_COLOR = '#333333';
+export const BLACK_COLOR = '#444444';
 export const RED_COLOR = '#FA4040';
