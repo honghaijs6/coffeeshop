b9994df869962df45b959b4148ac22c25dafb125
Hồng Hải <honghai.js6@gmail.com>
2019-01-24 18:23:06 +0700

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,33 +12,36 @@ class login extends Component {
 
             <ImageBackground source={require('../../assets/images/bg.jpg')} style={{width: '100%', height: '100%'}}>
 
-                <Container 
+                <Container
                 style={{
-                    
+
                     flex:1,
                     backgroundColor:'rgba(87,60,35,0.8)',
-                    
+
 
                 }}>
 
                 <Content>
-                    
+
+
                     <View style={{
                         width:'80%',
-                        marginTop:'45%',
+                        marginTop:'15%',
                         alignSelf:'center',
                         justifyContent:'space-between'
                     }}>
+                        <ImageBackground source={require('../../assets/mylogo.png')} style={{width: 80, height: 106, alignSelf: 'center', marginBottom: 20}} />
+
 
                         <View style={{
                             justifyContent:'space-between',
                             height:120
                         }}>
-                            <Item style={ s.item}> 
+                            <Item style={ s.item}>
                                 <Icon style={{ color:'#fff' }} name='mail' />
                                 <Input placeholderTextColor="rgba(255,255,255,0.3)" style={{ color:'#ffffff'}} placeholder='E-mail'/>
                             </Item>
-                            
+
                             <Item style={ s.item}>
                                 <Icon style={{ color:'#fff' }} name='unlock' />
                                 <Input secureTextEntry={true} placeholderTextColor="rgba(255,255,255,0.3)" style={{ color:'#ffffff'}}   placeholder='Password'/>
@@ -48,41 +51,41 @@ class login extends Component {
                         <View style={{
                             marginTop:'15%',
                             justifyContent:'space-between',
-                            height:120
+                            height:140
                         }}>
                             <Button full style={s.button}>
-                                <Text style={s.text}> Login </Text>
+                                <Text style={[s.text, {color: 'rgba(87,60,35,0.8)'}]}> Login </Text>
                             </Button>
 
                             <Button full style={[s.button,{backgroundColor:'rgba(68,103,176,0.6)'}]}>
                                 <Icon style={{ color:'#fff' }} name='logo-facebook' />
                                 <Text style={s.text}> Login with Facebook </Text>
                             </Button>
-                        
+
                         </View>
-                        
+
                         <View style={{
                                 alignItems:'center',
                                 paddingTop:40
                             }}>
                              <Text style={s.text}> Don't have an account? </Text>
                              <Link>
-                               
+
                                 <Text style={ s.text}> Sign up  </Text>
                             </Link>
-                                
+
                         </View>
 
                     </View>
-                
-                </Content> 
-               
-            </Container>    
-                
+
+                </Content>
+
+            </Container>
+
             </ImageBackground>
-                
-            
-      
+
+
+
         );
     }
 }
@@ -97,12 +100,12 @@ const s = StyleSheet.create({
         height:50
     },
     input:{
-        
+
     },
-    item:{ 
+    item:{
        borderWidth:0.5,
        borderBottomColor:'rgba(255,255,255,0.5)'
-    
+
     },
-    
+
 });
