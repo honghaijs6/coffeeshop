39b2ccdd02c1aed239e6a7d4f2ef267860791982
Hồng Hải <honghai.js6@gmail.com>
2019-01-26 12:12:35 +0700

    Create foods.js

---
+++ src/pages/menu/foods.js
@@ -0,0 +1,62 @@
+/* @flow weak */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+import {RED_COLOR, COFFEE_COLOR, BLACK_COLOR, GREY_COLOR } from '../../config/const';
+
+import {  Icon, Content } from 'native-base';
+import BenBody from '../../components/BenBody';
+import NoData from '../../components/NoData';
+
+import drinksData from './data.json';
+
+
+
+export default class BodyFoods extends Component{
+
+  render(){
+
+
+    return(
+      <Content style={{
+          backgroundColor: GREY_COLOR
+        }}>
+
+        <BenBody>
+
+            <NoData icon="pizza" message="we have no food menu yet..." />
+
+        </BenBody>
+
+      </Content>
+    )
+  }
+}
+
+const s =  StyleSheet.create({
+  h4:{
+    color: COFFEE_COLOR,
+    fontSize: 20,
+    fontWeight: 'bold',
+
+  },
+  price:{
+    color:RED_COLOR,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  txt:{
+    fontSize: 16,
+    color: BLACK_COLOR,
+    marginBottom:10,
+    fontFamily: 'Roboto',
+
+  }
+})
