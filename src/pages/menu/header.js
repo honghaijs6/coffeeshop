56c2398da9fbd736d100fa8249dced8a832d50aa
Hồng Hải <honghai.js6@gmail.com>
2019-01-26 12:12:39 +0700

    Create header.js

---
+++ src/pages/menu/header.js
@@ -0,0 +1,37 @@
+/* @flow weak */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput
+} from 'react-native';
+
+import BenHeader from '../../components/BenHeader';
+import BackButton from '../../components/BackButton'
+
+
+const MyComponent = (props) => (
+  <BenHeader type="single">
+      <BackButton/>
+
+      <TextInput style={{
+        borderWidth: 0.5,
+        borderColor: 'rgba(0,0,0,0.2)',
+        width: '86%',
+        height: 36,
+        paddingHorizontal: 10,
+        borderRadius: 20
+      }} placeholder="Search" />
+
+  </BenHeader>
+);
+
+export default MyComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
