ed00ae7b90960792dfb2b2a04481a0d0184564b9
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2019-01-28 08:04:01 +0700

    Update const.js

--- src/config/const.js
+++ src/config/const.js
@@ -2,3 +2,5 @@ export const COFFEE_COLOR = '#18A689';
 export const GREY_COLOR = '#F0F0F0';
 export const BLACK_COLOR = '#444444';
 export const RED_COLOR = '#FA4040';
+
+export const AVATAR_URL = 'https://firebasestorage.googleapis.com/v0/b/coffee-shop-229518.appspot.com/o/images%2Favatar.jpg?alt=media&token=1194edcd-f16c-4f4f-ba47-fb35298903b6';
