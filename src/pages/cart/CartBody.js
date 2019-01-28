1e02a350a89c397b7b9bb42081bc0918f3694f82
Hồng Hải <honghai.js6@gmail.com>
2019-01-28 18:56:57 +0700

    Update CartBody.js

--- src/pages/cart/CartBody.js
+++ src/pages/cart/CartBody.js
@@ -1,11 +1,12 @@
 /* @flow weak */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 import { Icon } from 'native-base';
@@ -15,97 +16,131 @@ import { GREY_COLOR, COFFEE_COLOR, RED_COLOR, BLACK_COLOR } from '../../config/c
 import CartItem from './CartItem';
 
 
-const CartBody = ({}) => (
-  <View>
-      <View style={{margin: 10}}>
-        <Text style={{ fontFamily: 'Roboto'}}> Delivery infomation </Text>
-      </View>
+export default class  CartBody extends Component{
 
-      <View style={{
-        padding: 10,
-        backgroundColor: '#fff'
-      }}>
+  constructor(props){
+    super(props);
 
-        {/*ROW*/}
-        <View style={ s.row }>
+    this.state = {
+      userInfo:props.userInfo
+    }
 
-          <Icon style={s.icon} name="contact" />
-          <TextInput placeholder="Your name" value="Benjamin HD" style={s.input}  />
+  }
 
-        </View>
+  calculateBill(data){
 
-        <View style={ s.row }>
+    let total = 0
+    data.map((item)=>{
+       total += item.amount * item.price
 
-          <Icon style={s.icon} name="call" />
-          <TextInput placeholder="Your phone number" value="0916396260" style={s.input}  />
+       console.log(total);
 
-        </View>
+    });
 
-        <View style={ s.row }>
+    return total;
+  }
+  render(){
 
-          <Icon style={s.icon} name="pin" />
-          <TextInput  placeholder="Delivery address" style={s.input}  />
+    return(
+      <View>
+          <View style={{margin: 10}}>
+            <Text style={{ fontFamily: 'Roboto'}}> Delivery infomation </Text>
+          </View>
 
-        </View>
-
-      </View>
-
-      {/* BLOCK 2 */}
-      <View style={{margin: 10}}>
-        <Text style={{ fontFamily: 'Roboto'}}> Your Orders detail </Text>
-      </View>
-
-      <View style={s.block}>
-
-          <CartItem data={{name:"Black Milk tea", amount:1, price:12 }} />
-          <CartItem data={{name:"Green Milk tea", amount:2, price:12 }} />
-
-          {/* TOTAL */}
           <View style={{
-            padding: 0,
-            marginTop: 10,
-            justifyContent: 'space-between',
-            flexDirection: 'row',
+            padding: 10,
+            backgroundColor: '#fff'
           }}>
 
-            <View>
-              <Text style={s.txt}> Total </Text>
+            {/*ROW*/}
+            <View style={ s.row }>
+
+              <Icon style={s.icon} name="contact" />
+              <TextInput onChangeText={(text)=>{ this.props.onChangeText({name:text}) }} placeholder="Your name" value={ this.props.userInfo.name } style={s.input}  />
+
             </View>
 
-            <View>
-              <Text style={[s.txt, {fontSize: 20, color: COFFEE_COLOR}]}> 11 $ </Text>
+            <View style={ s.row }>
+
+              <Icon style={s.icon} name="call" />
+              <TextInput keyboardType = "number-pad" onChangeText={(text)=>{ this.props.onChangeText({phone:text}) }}  placeholder="Your phone number" value={ this.props.userInfo.phone } style={s.input}  />
+
+            </View>
+
+            <View style={ s.row }>
+
+              <Icon style={s.icon} name="pin" />
+              <TouchableOpacity style={{
+                  width: '90%'
+                }}>
+                <Text> { this.state.userInfo.recent_address } </Text>
+              </TouchableOpacity>
+
             </View>
 
           </View>
 
-          {/* visa master cart */}
-          <View style={{
-            paddingVertical: 10,
-            borderTopColor: 'rgba(0,0,0,0.2)',
-            borderTopWidth: 0.5,
-            padding: 0,
-            marginTop: 10,
-            justifyContent: 'space-between',
-            flexDirection: 'row',
-          }}>
-
-            <View style={{flexDirection: 'row'}}>
-              <Icon name="card" style={{fontSize: 20, color:COFFEE_COLOR, marginRight: 10, marginLeft: 5}} />
-              <Text style={s.txt}> Visa/Master/JCB </Text>
-            </View>
-
-            <View>
-              <Text>Setup </Text>
-            </View>
-
+          {/* BLOCK 2 */}
+          <View style={{margin: 10}}>
+            <Text style={{ fontFamily: 'Roboto'}}> Your Orders detail </Text>
           </View>
 
+          <View style={s.block}>
+
+              {
+                this.props.data.map((item,index)=>{
+                  return(
+                    <CartItem key={index} data={{name:item.name, amount:item.amount, price: item.amount * item.price  }} />
+                  )
+                })
+              }
+
+
+              {/* TOTAL */}
+              <View style={{
+                padding: 0,
+                marginTop: 10,
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+              }}>
+
+                <View>
+                  <Text style={s.txt}> Total </Text>
+                </View>
+
+                <View>
+                  <Text style={[s.txt, {fontSize: 20, color: COFFEE_COLOR}]}> { this.calculateBill(this.props.data) } $ </Text>
+                </View>
+
+              </View>
+
+              {/* visa master cart */}
+              <View style={{
+                paddingVertical: 10,
+                borderTopColor: 'rgba(0,0,0,0.2)',
+                borderTopWidth: 0.5,
+                padding: 0,
+                marginTop: 10,
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+              }}>
+
+                <View style={{flexDirection: 'row'}}>
+                  <Icon name="card" style={{fontSize: 20, color:COFFEE_COLOR, marginRight: 10, marginLeft: 5}} />
+                  <Text style={s.txt}> Visa/Master/JCB </Text>
+                </View>
+
+                <View>
+                  <Text>Setup </Text>
+                </View>
+
+              </View>
+
+          </View>
       </View>
-  </View>
-);
-
-export default CartBody;
-
+    )
+  }
+}
 
 const s = StyleSheet.create({
 
