89b564ca8b2661a192289ecd848a41f09ce35911
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2019-01-28 08:04:19 +0700

    Create authen.js

---
+++ src/model/authen.js
@@ -0,0 +1,165 @@
+import {auth, database, provider} from "../config/firebase";
+import {AsyncStorage} from 'react-native';
+import { AVATAR_URL } from '../config/const';
+
+import store from '../redux/store';
+
+export const benAuth = {
+
+  localStorage:{
+
+  },
+
+  store:store,
+
+  isLoged(){
+    // DETECT localStorage ;
+
+  },
+
+  _whereStateChange(newState){
+
+    store.dispatch(newState);
+  },
+
+  register(data,onSuccess,onError) {
+
+
+    auth.createUserWithEmailAndPassword(data.email, data.password)
+        .then((resp) => {
+
+            console.log(resp);
+
+            data.uid = resp.user.uid;
+
+            data.createdAt = new Date().getTime();
+            data.updatedAt = data.createdAt;
+            data.photoURL = AVATAR_URL;
+
+            /*data.createdAt  = resp.user.createdAt;
+            data.lastLoginAt = resp.user.lastLoginAt;
+            data.photoURL = resp.user.photoURL;
+            data.phoneNumber = resp.user.phoneNumber;
+            */
+
+
+            const userRef = database.ref().child('users');
+
+            userRef.child(data.uid).update({...data})
+                .then(() => {
+                    onSuccess(data);
+                })
+                .catch((error) => { onError(error); });
+        })
+        .catch((error) => { onError(error); } );
+  },
+
+  doLogin(data,onSuccess,onError){
+
+    const {email, password} = data;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then((resp) => {
+            //Get the user object from the realtime database
+            let {user} = resp;
+
+
+
+            database.ref('users').child(user.uid).once('value')
+                .then((snapshot) => {
+
+                    const exists = (snapshot.val() !== null);
+
+                    //if the user exist in the DB, replace the user variable with the returned snapshot
+
+                    if (exists) user = snapshot.val();
+
+                    //if (exists) dispatch({type: t.LOGGED_IN, user});
+
+                    this._whereStateChange({
+                      type:'LOGIN',
+                      isLoggedIn:true,
+                      userInfo:user
+                    });
+
+                    onSuccess({exists, user});
+                })
+                .catch((error) => { onError(error)  });
+        })
+        .catch((error) => {  onError(error) }  );
+
+  },
+
+  doSignOut(onSuccess,onError){
+    auth.signOut()
+        .then(() =>  {
+
+          this._whereStateChange({
+            type:'LOGIN',
+            isLoggedIn:false
+          })
+          onSuccess(true)
+        } )
+        .catch((error) => onError(error));
+  },
+
+  checkLoginStatus(callback) {
+
+
+
+    auth.onAuthStateChanged((user) => {
+        let isLoggedIn = (user !== null);
+
+        if (isLoggedIn) {
+
+            //Get the user object from the realtime database
+            database.ref('users').child(user.uid).once('value')
+                .then((snapshot) => {
+
+                    const exists = (snapshot.val() !== null);
+
+                    //if the user exist in the DB, replace the user variable with the returned snapshot
+                    if (exists) user = snapshot.val();
+                    //if (exists) dispatch({type: t.LOGGED_IN, user});
+
+                    callback(exists, isLoggedIn);
+                })
+                .catch((error) => {
+                    //unable to get user
+                    //dispatch({type: t.LOGGED_OUT});
+                    callback(false, false);
+                });
+        } else {
+            //dispatch({type: t.LOGGED_OUT});
+            callback(false, isLoggedIn)
+        }
+    });
+  },
+
+  signInWithFacebook(fbToken,) {
+    return new Promise((resolve, reject) => {
+        const credential = provider.credential(fbToken);
+        auth.signInWithCredential(credential)
+            .then((user) => {
+                //Get the user object from the realtime database
+                database.ref('users').child(user.uid).once('value')
+                    .then((snapshot) => {
+
+                        const exists = (snapshot.val() !== null);
+
+                        //if the user exist in the DB, replace the user variable with the returned snapshot
+                        if (exists) user = snapshot.val();
+
+                        //if (exists) dispatch({type: t.LOGGED_IN, user});
+
+                        resolve({exists, user});
+                    })
+                    .catch((error) => reject(error));
+            })
+            .catch((error) => reject(error));
+    });
+  }
+
+
+
+}
