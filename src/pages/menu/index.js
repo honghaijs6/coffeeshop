288f035df6eeeb9061ef3438bd5e79140d9de0c1
Hồng Hải <honghai.js6@gmail.com>
2019-01-28 18:57:27 +0700

    Update index.js

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -12,10 +12,24 @@ import {
 import { Container,  Content } from 'native-base';
 import { GREY_COLOR, COFFEE_COLOR } from '../../config/const' ;
 
+import products from '../../data/products.json';
 
 import MenuHeader from './header';
 import MenuBody from './body'
 
+function ButtonOrder (props){
+
+  return(
+    <TouchableOpacity onPress={ ()=> { props.onPress() } } style={{
+        height: 55,
+        backgroundColor: COFFEE_COLOR,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text style={{ fontFamily: 'Roboto', fontSize: 18, color:'#fff'}}> Order Now  </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class Menu extends Component {
 
@@ -28,25 +42,70 @@ export default class Menu extends Component {
       onAction:'',
       tab:'menu',
 
-      products:[]
+      category:'milktea',
+      data:products // data info recievr from order page
 
     }
   }
 
+
+  _onBackBtnPress(){
+    this._whereStateChange({
+      onAction:'change_tab',
+      toTab:'order'
+    })
+  }
+
+  _whereStateChange(newState){
+    this.props.onStateChange(newState);
+  }
+
+  _onPressItem(data){
+
+
+    this._whereStateChange({
+      onAction:'change_tab',
+      toTab:'productitem',
+      proInfo:data,
+    });
+
+  }
+
+  _onPressOrder(){
+    this._whereStateChange({
+      onAction:'change_tab',
+      toTab:'cart'
+    });
+  }
   render() {
+
+
+
+    if(this.props.onTab === this.state.tab){
+
+      const data = this.state.data[this.props.data.code];
+
+      return(
+        <Container style={{
+          backgroundColor:GREY_COLOR,
+        }}>
+
+          <MenuHeader onBackBtnPress={()=>{ this._onBackBtnPress() }} />
+
+          <MenuBody onPressItem={(item)=>{ this._onPressItem(item) }}  data={ data } />
+
+          { this.props.shopingCart.length > 0 ? <ButtonOrder onPress={()=>{  this._onPressOrder() }} /> : null }
+
+
+
+        </Container>
+      )
+    }
+
     return (
-      <Container style={{
-        backgroundColor:GREY_COLOR,
-        display:  this.props.onTab === this.state.tab ? 'block':'none'
-      }}>
+      <View></View>
+    )
 
-        <MenuHeader />
-
-        <MenuBody />
-
-
-      </Container>
-    );
   }
 }
 
