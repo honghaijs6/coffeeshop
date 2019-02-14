46a86a6fcfd6bccfde2bb7d493455d81bc7558f7
Hồng Hải <honghai.js6@gmail.com>
2019-02-14 15:29:08 +0700

    Update index.js

---
+++ src/pages/mapPage/index.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+import { Container, Content, Icon } from 'native-base';
+
+
+
+export default class MapPage extends Component {
+  render() {
+    return (
+      <Container>
+
+      </Container
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
